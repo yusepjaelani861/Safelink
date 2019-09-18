0b14ee452a10dcd2ccb5893cc495bae69ab2785b
yusepjaelani861 <47809032+yusepjaelani861@users.noreply.github.com>
2019-09-18 07:43:32 +0700

    Update eduhighschool.js

--- eduhighschool.js
+++ eduhighschool.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.eduhighschool.tech/p/safelink_17.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.eduhighschool.tech/p/safelink_17.html?url=" + encryptedUrl; "https://www.eduhighschool.tech/2019/07/ruang-lingkup-biologi.html?url=" + encryptedUrl; "https://www.eduhighschool.tech/2019/07/metode-ilmiah.html?url=" + encryptedUrl; "https://www.eduhighschool.tech/2019/07/prosedur-keselamatan-kerja-di.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
